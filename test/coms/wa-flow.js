17f008c1f1e859bf464113e8c4971ccfa873bb39
marty <marty.shu@gmail.com>
2023-04-19 14:34:47 +0800

    first ver - test

--- test/coms/wa-flow.js
+++ test/coms/wa-flow.js
@@ -1,61 +1,164 @@
 import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js'
 
 class WAFlowchart extends LitElement {
-
-  constructor() {
-    super();
+  static get properties() {
+    return {
+      scale: { type: Number },
+      offsetX: { type: Number },
+      offsetY: { type: Number },
+      isDragging: { type: Boolean },
+      startX: { type: Number },
+      startY: { type: Number },
+    };
   }
-
   static get styles() {
     return css`
-      :host {
-        --my-element-margin: 0px;
-        margin: var(--my-element-margin);
-      }
-    `;
+        /* Define div styles */
+        #container {
+          width: 800px;
+          height: 600px;
+          border: 1px solid black;
+          position: relative;
+          overflow: hidden;
+        }
+         /* Define image styles */
+        #image {
+          width: 320px;
+          height: 240px;
+          position: absolute;
+          left: 25%;
+          top: 25%;
+          cursor: move;
+        }
+         /* Define button styles */
+        #zoom-in,
+        #zoom-out {
+          position: absolute;
+          bottom: 10px;
+          padding: 10px;
+          font-size: 20px;
+          background-color: white;
+          border: none;
+          cursor: pointer;
+        }
+         #zoom-in {
+          right: 60px;
+        }
+         #zoom-out {
+          right: 10px;
+        }
+      `;
+  }
+  constructor() {
+    super();
+    this.scale = 1;
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.startX = 0;
+    this.startY = 0;
+    this.isDragging = false;
   }
 
   firstUpdated() {
-    this.ele = this.shadowRoot;
+    this.container = this.shadowRoot.getElementById('container');
+    this.image = this.shadowRoot.getElementById('image');
   }
 
   render() {
-    return html`<h1 style="color:#ccc">流程圖</h1>`;
+    return html`
+        <div id="container">
+          <img
+            id="image"
+            src="https://images.unsplash.com/photo-1535026793569-5a810fda9660"
+            @mousedown="${this.onMouseDown}"
+            @mousemove="${this.onMouseMove}"
+            @mouseup="${this.onMouseUp}"
+          />
+          <button id="zoom-in" @click="${this.zoomIn}">+</button>
+          <button id="zoom-out" @click="${this.zoomOut}">-</button>
+        </div>
+      `;
   }
-
-  processing() {
-    const div = document.createElement('div');
-    this.clear();
-    this.ele.appendChild(div);
-    div.style['width'] = '100px';
-    div.style['font-size'] = '32px';
-    div.style['color'] = '#ccc';
-    div.style.display = 'flex';
-    div.style.justifyContent = 'center';
-    div.style.alignItems = 'center';
-    const texts = ['.', '..', '...'];
-    let index = 0;
-    this.intervalId = setInterval(() => {
-      div.textContent = `${texts[index]}`;
-      index = (index + 1) % texts.length;
-    }, 250);
+  onMouseDown(e) {
+    this.isDragging = true;
+    this.startX = e.clientX;
+    this.startY = e.clientY;
+    let image = this.image;
+    this.offsetX = image.offsetLeft;
+    this.offsetY = image.offsetTop;
   }
-
-  clear() {
-    clearInterval(this.intervalId);
-    while (this.ele.firstChild) {
-      this.ele.removeChild(this.ele.firstChild);
+  onMouseMove(e) {
+    if (this.isDragging) {
+      let dx = e.clientX - this.startX;
+      let dy = e.clientY - this.startY;
+      let image = this.image;
+      image.style.left = this.offsetX + dx + 'px';
+      image.style.top = this.offsetY + dy + 'px';
+      this.checkBounds();
     }
   }
 
-  setCode(code) {
-    try {
-      this.graph = code;
-      const imgEle = Viz(this.graph, { format: 'png-image-element', engine: 'dot' });
-      this.clear();
-      this.ele.appendChild(imgEle);
-    } catch (e) {
-      //console.log("flow:", e);
+  onMouseUp(e) {
+    this.isDragging = false;
+  }
+
+  zoomIn() {
+    let image = this.image;
+    let container = this.container;
+    // Calculate the image's center point before scaling
+    var centerX = (image.offsetLeft - container.offsetLeft) + image.offsetWidth / 2;
+    var centerY = (image.offsetTop - container.offsetTop) + image.offsetHeight / 2;
+
+    // Increase the scale and update image's width and height
+    this.scale += 0.1;
+    image.style.width = this.scale * image.offsetWidth + "px";
+    image.style.height = this.scale * image.offsetHeight + "px";
+
+    // Update image's left and top attributes to maintain the center point
+    image.style.left = (container.offsetLeft + centerX) - image.offsetWidth / 2 + "px";
+    image.style.top = (container.offsetTop + centerY) - image.offsetHeight / 2 + "px";
+
+    // Check if the image is out of the div's bounds
+    this.checkBounds();
+  }
+
+  zoomOut() {
+    let image = this.image;
+    let container = this.container;
+
+    // Calculate the image's center point before scaling
+    var centerX = (image.offsetLeft - container.offsetLeft) + image.offsetWidth / 2;
+    var centerY = (image.offsetTop - container.offsetTop) + image.offsetHeight / 2;
+
+    // Decrease the scale and update image's width and height
+    this.scale -= 0.1;
+    image.style.width = this.scale * image.offsetWidth + "px";
+    image.style.height = this.scale * image.offsetHeight + "px";
+
+    // Update image's left and top attributes to maintain the center point
+    image.style.left = (container.offsetLeft + centerX) - image.offsetWidth / 2 + "px";
+    image.style.top = (container.offsetTop + centerY) - image.offsetHeight / 2 + "px";
+
+    // Check if the image is out of the div's bounds
+    this.checkBounds();
+  }
+
+  checkBounds() {
+    let image = this.image;
+    let container = this.container;
+    // 如果圖片的左側或上側超出了 div 的範圍，則將其設置為最小的值 
+    if (image.offsetLeft < container.offsetLeft) {
+      //image.style.left = container.offsetLeft + "px";
+    }
+    if (image.offsetTop < container.offsetTop) {
+      //image.style.top = container.offsetTop + "px";
+    }
+    // 如果圖片的右側或下側超出了 div 的範圍，則將其設置為最大的值 
+    if (image.offsetLeft + image.offsetWidth > container.offsetLeft + container.offsetWidth) {
+      //image.style.left = container.offsetLeft + container.offsetWidth - image.offsetWidth + "px";
+    }
+    if (image.offsetTop + image.offsetHeight > container.offsetTop + container.offsetHeight) {
+      //image.style.top = container.offsetTop + container.offsetHeight - image.offsetHeight + "px";
     }
   }
 }
