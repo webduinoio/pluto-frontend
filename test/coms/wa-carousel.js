0d148b0b6a04bbd399de436fd265eccfb8d4ea36
Marty <marty@kingkit101.com>
2023-04-20 08:13:07 +0800

    fix previous,next btn

--- test/coms/wa-carousel.js
+++ test/coms/wa-carousel.js
@@ -1,15 +1,15 @@
 import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js'
 
 class ImageSlider extends LitElement {
-    static get properties() {
-        return {
-            index: { type: Number },
-            images: { type: Array },
-        };
-    }
+  static get properties() {
+    return {
+      index: { type: Number },
+      images: { type: Array },
+    };
+  }
 
-    static get styles() {
-        return css`
+  static get styles() {
+    return css`
           :host {
             display: block;
           }
@@ -77,51 +77,64 @@ class ImageSlider extends LitElement {
             background-color: white;
           }
         `;
-    }
+  }
 
-    constructor() {
-        super();
-        this.selectCallback = function () { };
-        this.index = 0;
-        this.images = [
-          ['python', 'https://md.webduino.io/uploads/upload_44bd52bfc96a03cba7e9002db46d9996.png','100px'],
-          ['webbit', 'https://webbit.webduino.io/blockly/simulator/dist/preview-bit.3c165d12.png','100px'],
-        ];
-    }
+  constructor() {
+    super();
+    this.selectCallback = function () { };
+    this.index = 0;
+    this.lastIdx = -1;
+    this.images = [
+      ['python', 'https://md.webduino.io/uploads/upload_44bd52bfc96a03cba7e9002db46d9996.png', '100px'],
+      ['webbit', 'https://webbit.webduino.io/blockly/simulator/dist/preview-bit.3c165d12.png', '100px'],
+    ];
+  }
 
-    render() {
-        return html`
+  firstUpdated() {
+    this.next = this.shadowRoot.getElementById('next');
+    this.previous = this.shadowRoot.getElementById('previous');
+    this.switchImg();
+  }
+
+  render() {
+    return html`
           <div class="container">
-            <span id="previous" @click=${() => this.index--}>＜</span>
-            <span id="next" @click=${() => this.index++}>＞</span>
+            <span id="previous" @click=${() => this.switchImg(--this.index)}>＜</span>
+            <span id="next" @click=${() => this.switchImg(++this.index)}>＞</span>
             <div id="slider" class="slider">
               ${this.images.map((image) => html`<img style="max-width: 100%; width:100%;height:auto" src=${image[1]} />`)}
             </div>
           </div>
         `;
-    }
+  }
 
-    select(callback) {
-        this.selectCallback = callback;
-    }
+  select(callback) {
+    this.selectCallback = callback;
+  }
 
-    updated(changedProperties) {
-        if (this.index >= this.images.length) {
-            this.index--;
-            return;
-        }
-        if (this.index < 0) {
-            this.index++;
-            return;
-        }
-        if (changedProperties.has('index') && this.lastIdx != this.index) {
-            this.lastIdx = this.index;
-            this.selectCallback(this.index, this.images[this.index][0]);
-            const imgWidth = this.shadowRoot.querySelector('.slider img').clientWidth;
-            const recomputedWidth = this.index * imgWidth;
-            this.shadowRoot.querySelector('.slider').scrollTo({ left: recomputedWidth, behavior: 'smooth' });
-        }
+  switchImg() {
+    this.index = this.index < 0 ? 0 : this.index;
+    this.index = this.index == this.images.length ? this.images.length - 1 : this.index;
+
+    if (this.lastIdx != this.index) {
+      this.lastIdx = this.index;
+
+      if (this.index == 0) {
+        this.previous.style['display'] = 'none';
+      } else {
+        this.previous.style['display'] = 'block';
+      }
+      if (this.index == this.images.length - 1) {
+        this.next.style['display'] = 'none';
+      } else {
+        this.next.style['display'] = 'block';
+      }
+      this.selectCallback(this.index, this.images[this.index][0]);
+      const imgWidth = this.shadowRoot.querySelector('.slider img').clientWidth;
+      const recomputedWidth = this.index * imgWidth;
+      this.shadowRoot.querySelector('.slider').scrollTo({ left: recomputedWidth, behavior: 'smooth' });
     }
+  }
 }
 
 customElements.define('wa-carousel', ImageSlider);
