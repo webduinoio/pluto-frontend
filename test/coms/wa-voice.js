84ac1fad95f144ec663ff022942f21dd6e0f0380
marty <marty.shu@gmail.com>
2023-04-25 23:22:26 +0800

    Add animation effect to the recording button.

--- test/coms/wa-voice.js
+++ test/coms/wa-voice.js
@@ -29,12 +29,44 @@ class SpeechRecognition extends LitElement {
       #toggle-btn.disabled {
         background-color: #fcc;
         cursor: default;
+        animation: blink 1s infinite;
+      }
+      
+      @keyframes blink {
+        0% {
+          opacity: 1;
+        }
+        50% {
+          opacity: 0.6;
+        }
+        100% {
+          opacity: 1;
+        }
       }
 
       #toggle-btn svg {
         margin-right: 5px;
         fill: currentColor;
       }
+      .material-icons {
+        font-family: 'Material Icons', sans-serif;
+        font-size: 24px;
+        line-height: 24px;
+        font-weight: normal;
+        font-style: normal;
+        letter-spacing: normal;
+        text-transform: none;
+        display: inline-block;
+        white-space: nowrap;
+        word-wrap: normal;
+        direction: ltr;
+        -webkit-font-feature-settings: 'liga';
+        -webkit-font-smoothing: antialiased;
+      }
+      .effect{
+        background-color: #ffc;
+      }
+
     `;
   }
 
@@ -62,10 +94,8 @@ class SpeechRecognition extends LitElement {
       <button
         id="toggle-btn"
         class=${this.isRecording && !this.recognition ? '' : 'disabled'}
-        @click=${this.handleToggleBtnClick}
-      >
-      <svg fill="#000000" width="24px" height="24px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M960.315 96.818c-186.858 0-338.862 152.003-338.862 338.861v484.088c0 186.858 152.004 338.862 338.862 338.862 186.858 0 338.861-152.004 338.861-338.862V435.68c0-186.858-152.003-338.861-338.861-338.861M427.818 709.983V943.41c0 293.551 238.946 532.497 532.497 532.497 293.55 0 532.496-238.946 532.496-532.497V709.983h96.818V943.41c0 330.707-256.438 602.668-580.9 627.471l-.006 252.301h242.044V1920H669.862v-96.818h242.043l-.004-252.3C587.438 1546.077 331 1274.116 331 943.41V709.983h96.818ZM960.315 0c240.204 0 435.679 195.475 435.679 435.68v484.087c0 240.205-195.475 435.68-435.68 435.68-240.204 0-435.679-195.475-435.679-435.68V435.68C524.635 195.475 720.11 0 960.315 0Z" fill-rule="evenodd"></path> </g></svg>
-        開始錄音
+        @click=${this.handleToggleBtnClick} >
+        <span class="material-icons">mic</span>開始錄音
       </button>
     `;
   }
@@ -100,12 +130,13 @@ class SpeechRecognition extends LitElement {
         this.result = this.result + "\n";
         this.callback(this.result);
       };
-
+      this.voiceBtn.innerHTML = '<span class="material-icons" style="width:30px">interpreter_mode</span> 錄音中';
       this.recognition.start();
       this.isRecording = false;
       this.callback(this.result);
     } else {
       this.recognition.stop();
+      this.voiceBtn.innerHTML = '<span class="material-icons" >mic</span>開始錄音';
     }
   }
 }
