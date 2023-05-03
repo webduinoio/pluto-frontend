96c4372a546568ce24a4405922487c0e4908a8fb
Marty <marty@kingkit101.com>
2023-05-03 18:32:17 +0800

    modify markdown link

--- test/coms/wa-carousel.js
+++ test/coms/wa-carousel.js
@@ -131,9 +131,9 @@ class ImageSlider extends LitElement {
     this.index = 0;
     this.lastIdx = -1;
     this.images = [
-      ['python', 'Python 助教', '../coms/python_logo.png', '320px','https://md.webduino.io/XDR7n6FSRt6h9vy2RvX0pw?view'],
-      ['wbit', '控制 Web:Bit v2', '../coms/wv2.png', '320px','https://md.webduino.io/Rwzas6JrQKyM0XHRwQtRyQ?view'],
-      ['microbit', '控制 Micro:Bit', '../coms/microbit.png', '320px','https://md.webduino.io/Rwzas6JrQKyM0XHRwQtRyQ?view'],
+      ['python', 'Python 助教', '../coms/python_logo.png', '320px','https://md.webduino.io/s/jW7cOvXmg'],
+      ['wbit', '控制 Web:Bit v2', '../coms/wv2.png', '320px','https://md.webduino.io/s/3Jh3hCuJO'],
+      ['microbit', '控制 Micro:Bit', '../coms/microbit.png', '320px','https://md.webduino.io/s/JEuvsZRg-'],
       //['kebbi','../coms/kebbi.png', '320px'],
     ];
     this.actor = this.images[this.index][0];
