e3ddcbd6a13ddabc08cb3a20291df7be969c96e0
sheng <karokya@gmail.com>
2023-06-19 18:26:39 +0800

    feat: :hammer: add timestamp to index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import AutoImport from 'unplugin-auto-import/vite';
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          timestamp: new Date().toISOString(),
+        },
+      },
+    }),
     AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
