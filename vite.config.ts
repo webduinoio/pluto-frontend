3f7549f4c4b6535fbc8f9a481d4a97b6dbcdea3f
sheng <karokya@gmail.com>
2023-06-19 18:05:39 +0800

    feat: :hammer: add unplugin-auto-import for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: [
+        'vue',
+        'vue-router',
+        'vue-i18n',
+        {
+          axios: [['default', 'axios']],
+        },
+      ],
+      dts: 'src/types/auto-imports.d.ts', // typescript 宣告檔案位置
+      vueTemplate: false,
+    }),
+  ],
+});
