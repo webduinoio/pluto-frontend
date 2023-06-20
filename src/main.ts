116390457009c363e0b05c0f600c4501e1cc9c6d
sheng <karokya@gmail.com>
2023-06-20 09:27:40 +0800

    build: :hammer: add vite-plugin-vuetify

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,10 @@ import router from './router';
 // Vuetify
 import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import 'vuetify/styles';
 
 const vuetify = createVuetify({
-  components,
-  directives,
   icons: {
     defaultSet: 'mdi',
     aliases,
--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vuetify(),
     AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
