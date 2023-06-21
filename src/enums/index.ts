1190a9f9eab41813022a76b7ad638bf3dd0a6a54
sheng <karokya@gmail.com>
2023-06-21 15:21:09 +0800

    feat: :sparkles: add vee-validate

---
+++ src/enums/index.ts
@@ -0,0 +1,37 @@
+/**
+ * 待擴充 enums
+ */
+
+// for test
+// export enum MQTT_ACTORS {
+//   ENGLISH = 'eng',
+// }
+
+export enum LOCALE {
+  EN = 'en',
+  ZH_HANT = 'zh-hant',
+}
+
+// export enum MARKDOWN_TYPE {
+//   NONE = 'none',
+//   MARKDOWN = 'markdown',
+//   EXAM = 'exam',
+//   QUESTION = 'question',
+//   QUESTION_V2 = 'question_v2',
+// }
+
+// export enum ANSWER_RECORD_FROM {
+//   QUESTION = 'question',
+//   EXAM = 'exam-paper',
+// }
+
+// export enum ANSWER_RECORD_ERROR_TYPE {
+//   ERROR = 'AssertionError',
+//   TIMEOUT = 'TimeOutError',
+//   OUT_OF_MEMORY = 'OutOfMemoryError',
+// }
+
+// export enum RESULT_VIEW_MODE {
+//   LOCAL = 'local',
+//   SERVER = 'server',
+// }
