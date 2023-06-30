d2fac93e56159c3913438f9e9f3f8508741de637
GoGoPenguin <gogopenguin1995@gmail.com>
2023-06-30 16:49:40 +0800

    feat: :sparkles: update url to prod

--- test/html/oauth.js
+++ test/html/oauth.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://account-staging.webduino.io";
+const baseUrl = "https://account.webduino.io";
 const oauthClientID = "thlp6rstwsl4iqa4";
 
 const getCookie = (name) => {
@@ -34,13 +34,13 @@ const isLogin = async () => {
 }
 
 const logout = () => {
-    // https://account-staging.webduino.io/zh-TW/log-in?grant_type=authorization_code&response_type=code&client_id=thlp6rstwsl4iqa4&redirect_uri=http://localhost.webduino.io:8080/api/v1/auth/callback
+    // https://account.webduino.io/zh-TW/log-in?grant_type=authorization_code&response_type=code&client_id=thlp6rstwsl4iqa4&redirect_uri=http://localhost.webduino.io:8080/api/v1/auth/callback
     const locale = getCookie("NEXT_LOCALE") || "";
     const url = new URL(`${baseUrl}${locale ? `/${locale}` : ""}/log-in`);
     url.searchParams.append("grant_type", "authorization_code");
     url.searchParams.append("response_type", "code");
     url.searchParams.append("client_id", oauthClientID);
-    url.searchParams.append("redirect_uri", `https://chat-staging.webduino.io/api/v1/auth/callback`);
+    url.searchParams.append("redirect_uri", `https://chat.webduino.io/api/v1/auth/callback`);
 
     parent.location.href = url.toString();
 }
