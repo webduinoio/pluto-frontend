b4ee1a69197be5d206948c8c55de71b91c85136a
GoGoPenguin <gogopenguin1995@gmail.com>
2023-07-04 22:55:04 +0800

    build: :hammer: ssl on localhost

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import AutoImport from 'unplugin-auto-import/vite';
@@ -39,6 +40,7 @@ export default defineConfig({
         },
       },
     }),
+    basicSsl(),
   ],
   resolve: {
     alias: {
