50bb6f2d3749c37f791fdd653b10125dbc7e711b
GoGoPenguin <gogopenguin1995@gmail.com>
2023-07-05 16:05:36 +0800

    fix: :bug: fix unable to delete qa issue

--- src/views/ActorEditView.vue
+++ src/views/ActorEditView.vue
@@ -67,7 +67,7 @@ const onDeleteDataset = async (data: Dataset) => {
   try {
     showLoading();
     if (!data.id) return;
-    await deleteDataset(data.id);
+    await deleteDataset(data.id, data.actorId);
     await loadDataset();
   } catch (err: any) {
     console.error(err);
