05d2549fe42e18510e55d66670b6d3907f057ba4
sheng <karokya@gmail.com>
2023-07-05 09:46:32 +0800

    fix: :pencil2: fixed typo

--- src/services/oauth.ts
+++ src/services/oauth.ts
@@ -2,15 +2,15 @@
  * 後端，OAuth api
  */
 
-import { useLoginUrl } from '@/hooks/userOAuth';
+import { useLoginUrl } from '@/hooks/useOAuth';
 import axios from 'axios';
 
 // TODO: 之後需再優化
 function createInstance() {
-    return axios.create({
-        baseURL: import.meta.env.VITE_OAUTH_SERVER_HOST + '/api/v1',
-        withCredentials: true,
-    });
+  return axios.create({
+    baseURL: import.meta.env.VITE_OAUTH_SERVER_HOST + '/api/v1',
+    withCredentials: true,
+  });
 }
 
 const instance = createInstance();
@@ -19,24 +19,24 @@ const instance = createInstance();
  * 登出
  */
 export async function logout() {
-    try {
-        const config = {
-            method: 'delete',
-        };
-        await instance('/auth/logout', config);
-    } catch (error) {
-        console.error(error);
-    } finally {
-        location.href = useLoginUrl();
-    }
+  try {
+    const config = {
+      method: 'delete',
+    };
+    await instance('/auth/logout', config);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    location.href = useLoginUrl();
+  }
 }
 
 /**
  * 取得使用者
  */
 export function getUser() {
-    const config = {
-        method: 'get',
-    };
-    return instance('/user/', config);
+  const config = {
+    method: 'get',
+  };
+  return instance('/user/', config);
 }
