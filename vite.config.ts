d49ac7c0e60088bba598ebfc04b5ea156348c2e8
GoGoPenguin <gogopenguin1995@gmail.com>
2023-08-15 22:56:07 +0800

    feat: :sparkles: change favicon depend on env

--- vite.config.ts
+++ vite.config.ts
@@ -2,57 +2,62 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import AutoImport from 'unplugin-auto-import/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    vuetify(),
-    svgLoader({ defaultImport: 'component' }),
-    AutoImport({
-      include: [
-        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-        /\.vue$/,
-        /\.vue\?vue/, // .vue
-        /\.md$/, // .md
-      ],
-      imports: [
-        'vue',
-        'vue-router',
-        'vue-i18n',
-        {
-          axios: [['default', 'axios']],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      vue({
+        template: { transformAssetUrls },
+      }),
+      vuetify(),
+      svgLoader({ defaultImport: 'component' }),
+      AutoImport({
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue$/,
+          /\.vue\?vue/, // .vue
+          /\.md$/, // .md
+        ],
+        imports: [
+          'vue',
+          'vue-router',
+          'vue-i18n',
+          {
+            axios: [['default', 'axios']],
+          },
+        ],
+        dts: 'src/types/auto-imports.d.ts', // typescript 宣告檔案位置
+        vueTemplate: false,
+      }),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            timestamp: new Date().toISOString(),
+            favicon: env.VITE_FAVICON
+          },
         },
-      ],
-      dts: 'src/types/auto-imports.d.ts', // typescript 宣告檔案位置
-      vueTemplate: false,
-    }),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          timestamp: new Date().toISOString(),
-        },
+      }),
+      basicSsl(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
-    }),
-    basicSsl(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
-  test: {
-    // https://vitest.dev/guide/#configuring-vitest
-    globals: true,
-    environment: 'jsdom',
-    deps: {
-      inline: ['vuetify'],
+    test: {
+      // https://vitest.dev/guide/#configuring-vitest
+      globals: true,
+      environment: 'jsdom',
+      deps: {
+        inline: ['vuetify'],
+      },
     },
-  },
+  }
 });
