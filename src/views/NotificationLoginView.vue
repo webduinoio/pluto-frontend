804ffad17ba633cb3fac6f09cc8b9cc751723a4e
sheng <karokya@gmail.com>
2023-08-25 09:41:15 +0800

    feat: :sparkles: redirect back to that screen before performing logout

---
+++ src/views/NotificationLoginView.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { useSweetAlert } from '@/hooks/useSweetAlert';
+import { logout } from '@/services';
+
+const { fire, showLoading } = useSweetAlert();
+
+fire({
+  title: '登入並開始與小書僮對話',
+  text: '即將轉導回登入頁',
+  icon: 'warning',
+  confirmButtonText: '確認',
+  timer: 7000,
+  timerProgressBar: true,
+  allowOutsideClick: false,
+}).then(async () => {
+  showLoading();
+  logout();
+});
+</script>
+
+<template></template>
