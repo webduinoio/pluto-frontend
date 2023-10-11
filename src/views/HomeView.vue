4b958f6a49b7227134347bbf7c00d4c293b600f8
sheng <karokya@gmail.com>
2023-10-11 14:11:12 +0800

    style: :lipstick: add missed class

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -196,6 +196,7 @@ const onLoad = async ({ done }: { done: Function }) => {
             <v-row>
               <v-col v-for="item in data" :key="item.id" cols="auto">
                 <TheActor
+                  class="pa-2"
                   width="310"
                   height="380"
                   :data="item"
